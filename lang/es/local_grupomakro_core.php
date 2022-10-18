c263c99072caaf6c18391f1f00a97a5cf68b5d9d
GIlson <gilson.rincon@soluttoconsulting.com>
2022-10-18 18:24:03 +0000

    Initial commit

---
+++ lang/es/local_grupomakro_core.php
@@ -0,0 +1,25 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Solutto LMS Core is a plugin used by the various components developed by Solutto.
+ *
+ * @package    local_soluttolms_core
+ * @copyright  2022 Solutto Consulting <dev@soluttoconsulting.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Grupo Makro Core';
