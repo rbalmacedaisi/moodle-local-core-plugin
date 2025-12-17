525c85079322045a9fb3033189795a0433a90243
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2025-12-17 17:41:21 -0500

    feat: Add advanced course management page with filtering, statistics, and XLSX export for Moodle courses.

--- pages/manage_courses.php
+++ pages/manage_courses.php
@@ -2,6 +2,7 @@
 require_once(__DIR__ . '/../../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
 require_once($CFG->dirroot . '/course/lib.php');
+require_once($CFG->dirroot . '/vendor/autoload.php');
 
 // Permissions
 admin_externalpage_setup('grupomakro_core_manage_courses');
