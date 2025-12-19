a7e61a86921764d6c7a86745d65f6b1c277395e3
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2025-12-19 13:49:29 -0500

    refactor: update column check script to target `local_learning_courses` and remove `currentsubperiodid` check.

--- check_columns.php
+++ check_columns.php
@@ -3,15 +3,9 @@ define('CLI_SCRIPT', true);
 require(__DIR__ . '/../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
 
-$table = 'local_learning_users';
+$table = 'local_learning_courses';
 $columns = $DB->get_columns($table);
 echo "Columns in $table:\n";
 foreach ($columns as $col) {
     echo " - " . $col->name . " (Type: " . $col->type . ")\n";
-}
-
-if (!isset($columns['currentsubperiodid'])) {
-    echo "MISSING: currentsubperiodid\n";
-} else {
-    echo "FOUND: currentsubperiodid\n";
 }
