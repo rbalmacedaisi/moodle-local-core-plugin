cc1bb2c0c700b5a8388df38597fec47c42b1aac8
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2026-02-12 14:31:28 -0500

    fx

--- check_subperiod.php
+++ check_subperiod.php
@@ -10,7 +10,7 @@ echo "Checking subperiod data for a few students:\n";
 $sql = "SELECT lpu.userid, u.firstname, u.lastname, lpu.currentperiodid, lpu.currentsubperiodid, lpu.academicperiodid 
         FROM {local_learning_users} lpu 
         JOIN {user} u ON u.id = lpu.userid 
-        WHERE u.deleted = 0 AND u.suspended = 0 
+        WHERE u.email = 'a.enoc210415@gmail.com'
         LIMIT 10";
 $records = $DB->get_records_sql($sql);
 print_r($records);
