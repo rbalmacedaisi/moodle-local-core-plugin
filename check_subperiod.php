63c1147c93a74b329b764cb4b66160af19eb011d
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2026-02-12 14:29:57 -0500

    fx

--- check_subperiod.php
+++ check_subperiod.php
@@ -1,11 +1,22 @@
 <?php
-define('CLI_SCRIPT', true);
 require(__DIR__ . '/../../config.php');
+require_login();
+$context = context_system::instance();
+require_capability('moodle/site:config', $context);
 
-$sql = "SELECT lpu.userid, lpu.currentperiodid, lpu.currentsubperiodid, lpu.academicperiodid 
+echo "<pre>";
+echo "Checking subperiod data for a few students:\n";
+
+$sql = "SELECT lpu.userid, u.firstname, u.lastname, lpu.currentperiodid, lpu.currentsubperiodid, lpu.academicperiodid 
         FROM {local_learning_users} lpu 
         JOIN {user} u ON u.id = lpu.userid 
         WHERE u.deleted = 0 AND u.suspended = 0 
-        LIMIT 5";
+        LIMIT 10";
 $records = $DB->get_records_sql($sql);
 print_r($records);
+
+echo "\nChecking subperiods definition:\n";
+$subperiods = $DB->get_records('local_learning_subperiods');
+print_r($subperiods);
+
+echo "</pre>";
