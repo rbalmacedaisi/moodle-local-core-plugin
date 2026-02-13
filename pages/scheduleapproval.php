12593f2fad517deaecf09a85eb1bee495accfb7d
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2026-02-13 15:38:33 -0500

    fx

--- pages/scheduleapproval.php
+++ pages/scheduleapproval.php
@@ -103,7 +103,7 @@ echo <<<EOT
 <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
-  <div id="app">
+  <div id="gmk-app">
     <v-app class="transparent">
       <v-main>
         <div>
