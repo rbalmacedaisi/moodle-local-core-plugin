65eaff62393df39a85d0a1eb5693ccdd4660ea00
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2026-02-21 14:00:50 -0500

    fx

--- pages/academic_planning.php
+++ pages/academic_planning.php
@@ -31,6 +31,9 @@ echo $OUTPUT->header();
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js"></script>
 
+<!-- Excel Export Lib -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
+
 <!-- Scheduler Module Scripts -->
 <script src="../js/utils/scheduler_algorithm.js?v=<?= time() ?>"></script>
 <script src="../js/utils/pdfExport.js?v=<?= time() ?>"></script>
