0e84f5e8c119739c5788757221e479867fd47d81
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2026-02-24 14:28:29 -0500

    fx

--- pages/debug_persistence.php
+++ pages/debug_persistence.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . '/../../config.php');
+require_once(__DIR__ . '/../../../config.php');
 require_once($CFG->dirroot . '/local/grupomakro_core/locallib.php');
 
 require_login();
