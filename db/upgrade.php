e2cb58b80553c08005b7f30a55db0fab0b2bdb5d
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2026-08-03 10:59:10 -0500

    fix(schema): rename movement FK to movementlogfk to avoid name collision with indexes

--- db/upgrade.php
+++ db/upgrade.php
@@ -2935,9 +2935,8 @@ function xmldb_local_grupomakro_core_upgrade($oldversion) {
             $ltable->add_field('acted_at', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
 
             $ltable->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
-            $ltable->add_key('movementfk', XMLDB_KEY_FOREIGN, ['movement_id'], 'gmk_academic_movements', ['id']);
+            $ltable->add_key('movementlogfk', XMLDB_KEY_FOREIGN, ['movement_id'], 'gmk_academic_movements', ['id']);
             $ltable->add_key('userfk', XMLDB_KEY_FOREIGN, ['userid'], 'user', ['id']);
-            $ltable->add_index('movement_log_lookup_idx', XMLDB_INDEX_NOTUNIQUE, ['movement_id']);
 
             $dbman->create_table($ltable);
         }
