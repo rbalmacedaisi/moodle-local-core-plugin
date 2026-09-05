b6246cd7cb034b11d2c340c129b9193866a7d480
Ramiro Balmaceda <66266996+rjbj25@users.noreply.github.com>
2026-09-05 08:18:29 -0500

    chore(assets): bump cache-buster de grademodal/TeacherStudentTable
    Cambia el sufijo de version en la URL de grademodal.js y
    TeacherStudentTable.js para que el navegador descarte la copia en
    cache y recargue el JS con el umbral de aprobacion >= 71.
    
    - teacher_dashboard.php: ?v=20260901001/20260317002 -> ?v=20260905001
    - academicpanel.php: _20260902_letterscale -> _20260905_threshold71
    
    Sin este bump, los usuarios que ya cargaron la pagina seguirian
    viendo el bug aunque el pull al servidor tuviera la version
    corregida.

--- pages/academicpanel.php
+++ pages/academicpanel.php
@@ -218,6 +218,6 @@ $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/modals/
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/academicoffer.js?v=' . $assetversion));
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/curriculum.js?v=' . $assetversion));
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/revalidatestudents.js?v=' . $assetversion));
-$PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/modals/grademodal.js?v=' . $assetversion . '_20260902_letterscale'));
+$PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/modals/grademodal.js?v=' . $assetversion . '_20260905_threshold71'));
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/app.js?v=' . $assetversion));
 echo $OUTPUT->footer();
--- pages/teacher_dashboard.php
+++ pages/teacher_dashboard.php
@@ -36,10 +36,10 @@ $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/Teacher
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/ActivityCreationWizard.js?v=20260326001'), true);
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/ManageClass.js?v=20260713001_rev-revalida-cap'), true);
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/studenttable.js?v=20251231049'), true);
-$PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/TeacherStudentTable.js?v=20260901001'), true);
+$PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/TeacherStudentTable.js?v=20260905001'), true);
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/GradesGrid.js?v=20260827000'), true);
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/GradebookManager.js?v=20260312012'), true);
-$PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/modals/grademodal.js?v=20260317002'), true);
+$PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/modals/grademodal.js?v=20260905001'), true);
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/modals/attendancemodal.js?v=20251231049'), true);
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/PendingGradingView.js?v=20260320003'), true);
 $PAGE->requires->js(new moodle_url('/local/grupomakro_core/js/components/QuickGrader.js?v=20260409007'), true);
